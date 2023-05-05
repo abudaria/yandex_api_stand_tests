3d975880f831b900bc964a14e3ac99e159b68eb8
Alex Skuratov <Aleksandr.Skuratov@securrency.com>
2023-05-05 00:17:00 -0300

    Initial

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c6b3e844-cc16-4fd0-b77e-54dfb001fc6d.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/" 
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" 
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ create_kit_name_kit_test.py
@@ -0,0 +1,4 @@
+def test_sum():
+    sum = 2 + 2
+    assert sum == 4
+
